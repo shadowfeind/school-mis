abb6cb744621ffc8521815d773fc148f19df6e31
Kiran Rai <travestroy007@gmail.com>
2022-04-17 13:02:00 +0545

    small changes

--- mis/src/constants.js
+++ mis/src/constants.js
@@ -19,7 +19,6 @@ let userSession = sessionStorage.getItem("blueberrytoken")
 
 // let userSession =
 //   "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3ZTY4N2Q3Zi0zZGRkLTQ3NmMtYmMzNi1jZGY2N2ViNzhlMjAiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTA1MzMiLCJJRFVzZXIiOiIxMDUzMyIsIklEUm9sZSI6IjIiLCJNYXJrQXNBZG1pbiI6IlRydWUiLCJJREhSQnJhbmNoIjoiMSIsIklERGVwYXJ0bWVudCI6IjEiLCJjb21wYW55IjoiMiIsIklzVGVtcG9yYXJ5U2Vzc2lvbkVuYWJsZWQiOiJGYWxzZSIsIklzTmV3bHlBZGVkIjoiRmFsc2UiLCJJc0RlcGFydG1lbnRIZWFkIjoiRmFsc2UiLCJSZW1lbWJlck1lIjoiRmFsc2UiLCJGdWxsTmFtZSI6IkNyZWF0aW9uICBBY2FkZW15IiwicGlkUmVmRm9yRWRpdCI6IlJvb3RVc2VyIiwiZXhwIjoxNjQ4NzU5ODMxLCJpc3MiOiJodHRwOi8vbXlzaXRlLmNvbSIsImF1ZCI6Imh0dHA6Ly9teXNpdGUuY29tIn0.q6lZ1IfygRAuKGnNw0WpQvEkPYDpUe9NNBUNQnH1yI0";
-
 // let userRefreshToken = "T9P434k9704MVuPnR0ga0C6XXnlZLooPPX8qf68+90Q=";
 //for JWT token for dev mode
 export const tokenConfig = {
@@ -78,7 +77,7 @@ axiosInstance.interceptors.request.use(async (req) => {
 
   try {
     const { data } = await axios.post(
-      "http://vidyacube.com/api/RefreshTokenGenerator/RefreshToken",
+      "https://vidyacube.com/api/RefreshTokenGenerator/RefreshToken",
       JSONdata,
       config
     );

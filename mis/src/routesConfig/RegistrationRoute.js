14d04b017e872b1a9af85cd742609894f3faadbd
amir0123 <amirkarmacharya4@gmail.com>
2022-04-26 17:13:16 +0545

    icons changes

--- mis/src/routesConfig/RegistrationRoute.js
+++ mis/src/routesConfig/RegistrationRoute.js
@@ -3,6 +3,8 @@ import {
   PeopleOutline,
   ChromeReaderMode,
 } from "@material-ui/icons";
+import PortraitIcon from '@material-ui/icons/Portrait';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
 export const RegistrationRoute = [
   {
     name: "Counter Configuraion",
@@ -13,13 +15,13 @@ export const RegistrationRoute = [
   {
     name: "Admission Configuraion",
     route: "/admission-configuration",
-    icon: Accessible,
+    icon: PortraitIcon,
     exact: false,
   },
   {
     name: "Student Registration",
     route: "/student-registration",
-    icon: ChromeReaderMode,
+    icon: GroupAddIcon,
     exact: false,
   },
 ];

34f44bcbe0092a307fc1dae680caac1e5c1637cf
Kiran Rai <travestroy007@gmail.com>
2022-05-05 16:52:00 +0545

    changes

--- mis/src/dashboard/DashboardHeader.js
+++ mis/src/dashboard/DashboardHeader.js
@@ -4,6 +4,7 @@ import { School, PeopleOutline, Face, Settings } from "@material-ui/icons";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import StayCurrentPortraitIcon from "@material-ui/icons/StayCurrentPortrait";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   cardStyle: {
@@ -11,6 +12,10 @@ const useStyles = makeStyles((theme) => ({
     padding: "20px 20px 10px",
     borderRadius: "10px",
     boxShadow: "5px 5px 5px #d4d4d4",
+    "& a": {
+      textDecoration: "none",
+      color: "#000",
+    },
   },
   heading: {
     margin: "0",
@@ -34,145 +39,153 @@ const DashboardHeader = ({ dashboardTopContent }) => {
     <Grid container>
       <Grid item xs={3}>
         <Card className={classes.cardStyle}>
-          <Grid container alignItems="center" justifyContent="center">
-            <Grid item xs={9}>
-              <h4 className={classes.heading}>Total No. Of Teachers</h4>
-              <h1 className={classes.numberHeading}>
-                {dashboardTopContent && dashboardTopContent?.activeTeacher}
-              </h1>
+          <Link to={"/employee-management"}>
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid item xs={9}>
+                <h4 className={classes.heading}>Total No. Of Teachers</h4>
+                <h1 className={classes.numberHeading}>
+                  {dashboardTopContent && dashboardTopContent?.activeTeacher}
+                </h1>
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  backgroundColor: "#d14343",
+                  padding: "12px",
+                  borderRadius: "10px",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                <School style={{ fontSize: "30px", color: "#fff" }} />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                backgroundColor: "#d14343",
-                padding: "12px",
-                borderRadius: "10px",
-                textAlign: "center",
-              }}
-            >
-              {" "}
-              <School style={{ fontSize: "30px", color: "#fff" }} />
+            <Grid container alignItems="center">
+              <Grid item xs={1}>
+                <ArrowUpwardIcon
+                  style={{ fontSize: "25px", color: "#d14343" }}
+                />
+              </Grid>
+              <Grid item xs={11}>
+                <p className={classes.smallHeading}>5% Increase</p>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item xs={1}>
-              <ArrowUpwardIcon style={{ fontSize: "25px", color: "#d14343" }} />
-            </Grid>
-            <Grid item xs={11}>
-              <p className={classes.smallHeading}>5% Increase</p>
-            </Grid>
-          </Grid>
+          </Link>
         </Card>
       </Grid>
       <Grid item xs={3}>
-        <Card
-          className={classes.cardStyle}
-          // style={{ borderBottom: "3px solid #647acb" }}
-        >
-          <Grid container alignItems="center" justifyContent="center">
-            <Grid item xs={9}>
-              <h4 className={classes.heading}>Total No. Of Students</h4>
-              <h1 className={classes.numberHeading}>
-                {dashboardTopContent && dashboardTopContent?.activeStudent}
-              </h1>
+        <Card className={classes.cardStyle}>
+          <Link to={"/student-management"}>
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid item xs={9}>
+                <h4 className={classes.heading}>Total No. Of Students</h4>
+                <h1 className={classes.numberHeading}>
+                  {dashboardTopContent && dashboardTopContent?.activeStudent}
+                </h1>
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  backgroundColor: "#647acb",
+                  padding: "12px",
+                  borderRadius: "10px",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                <PeopleOutline style={{ fontSize: "30px", color: "#fff" }} />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                backgroundColor: "#647acb",
-                padding: "12px",
-                borderRadius: "10px",
-                textAlign: "center",
-              }}
-            >
-              {" "}
-              <PeopleOutline style={{ fontSize: "30px", color: "#fff" }} />
+            <Grid container alignItems="center">
+              <Grid item xs={1}>
+                <ArrowUpwardIcon
+                  style={{ fontSize: "25px", color: "#647acb" }}
+                />
+              </Grid>
+              <Grid item xs={11}>
+                <p className={classes.smallHeading}>10% Increase</p>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item xs={1}>
-              <ArrowUpwardIcon style={{ fontSize: "25px", color: "#647acb" }} />
-            </Grid>
-            <Grid item xs={11}>
-              <p className={classes.smallHeading}>10% Increase</p>
-            </Grid>
-          </Grid>
+          </Link>
         </Card>
       </Grid>
       <Grid item xs={3}>
-        <Card
-          className={classes.cardStyle}
-          // style={{ borderBottom: "3px solid #e9b949" }}
-        >
-          <Grid container alignItems="center" justifyContent="center">
-            <Grid item xs={9}>
-              <h4 className={classes.heading}>Total No. Of Subjects</h4>
-              <h1 className={classes.numberHeading}>
-                {dashboardTopContent && dashboardTopContent?.activeSubject}
-              </h1>
+        <Card className={classes.cardStyle}>
+          <Link to={"/academic-configuration"}>
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid item xs={9}>
+                <h4 className={classes.heading}>Total No. Of Subjects</h4>
+                <h1 className={classes.numberHeading}>
+                  {dashboardTopContent && dashboardTopContent?.activeSubject}
+                </h1>
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  backgroundColor: "#e9b949",
+                  padding: "12px",
+                  borderRadius: "10px",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                <MenuBookIcon style={{ fontSize: "30px", color: "#fff" }} />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                backgroundColor: "#e9b949",
-                padding: "12px",
-                borderRadius: "10px",
-                textAlign: "center",
-              }}
-            >
-              {" "}
-              <MenuBookIcon style={{ fontSize: "30px", color: "#fff" }} />
+            <Grid container alignItems="center">
+              <Grid item xs={1}>
+                <ArrowUpwardIcon
+                  style={{ fontSize: "25px", color: "#e9b949" }}
+                />
+              </Grid>
+              <Grid item xs={11}>
+                <p className={classes.smallHeading}>2% Increase</p>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item xs={1}>
-              <ArrowUpwardIcon style={{ fontSize: "25px", color: "#e9b949" }} />
-            </Grid>
-            <Grid item xs={11}>
-              <p className={classes.smallHeading}>2% Increase</p>
-            </Grid>
-          </Grid>
+          </Link>
         </Card>
       </Grid>
       <Grid item xs={3}>
-        <Card
-          className={classes.cardStyle}
-          // style={{ borderBottom: "3px solid #2cb1bc" }}
-        >
-          <Grid container alignItems="center" justifyContent="center">
-            <Grid item xs={9}>
-              <h4 className={classes.heading}>Total No. Of Mobile Users</h4>
-              <h1 className={classes.numberHeading}>
-                {dashboardTopContent && dashboardTopContent?.activeMobileUsers}
-              </h1>
+        <Card className={classes.cardStyle}>
+          <Link to={"/student-management"}>
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid item xs={9}>
+                <h4 className={classes.heading}>Total No. Of Mobile Users</h4>
+                <h1 className={classes.numberHeading}>
+                  {dashboardTopContent &&
+                    dashboardTopContent?.activeMobileUsers}
+                </h1>
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  backgroundColor: "#2cb1bc",
+                  padding: "12px",
+                  borderRadius: "10px",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                <StayCurrentPortraitIcon
+                  style={{ fontSize: "30px", color: "#fff" }}
+                />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                backgroundColor: "#2cb1bc",
-                padding: "12px",
-                borderRadius: "10px",
-                textAlign: "center",
-              }}
-            >
-              {" "}
-              <StayCurrentPortraitIcon
-                style={{ fontSize: "30px", color: "#fff" }}
-              />
+            <Grid container alignItems="center">
+              <Grid item xs={1}>
+                <ArrowUpwardIcon
+                  style={{ fontSize: "25px", color: "#2cb1bc" }}
+                />
+              </Grid>
+              <Grid item xs={11}>
+                <p className={classes.smallHeading}>4% Increase</p>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item xs={1}>
-              <ArrowUpwardIcon style={{ fontSize: "25px", color: "#2cb1bc" }} />
-            </Grid>
-            <Grid item xs={11}>
-              <p className={classes.smallHeading}>4% Increase</p>
-            </Grid>
-          </Grid>
+          </Link>
         </Card>
       </Grid>
     </Grid>

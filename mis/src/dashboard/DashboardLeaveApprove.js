e3dacc185f92d9736c7be8e4fcda2eb9e0dc55c0
Kiran Rai <travestroy007@gmail.com>
2022-05-06 11:04:39 +0545

    leave request color change

--- mis/src/dashboard/DashboardLeaveApprove.js
+++ mis/src/dashboard/DashboardLeaveApprove.js
@@ -203,7 +203,18 @@ const DashboardLeaveApprove = () => {
               <StyledTableCell align="left">
                 {s.FromDate?.slice(0, 10)} /<div>{s.ToDate?.slice(0, 10)}</div>
               </StyledTableCell>
-              <StyledTableCell align="left">{s.Status}</StyledTableCell>
+              <StyledTableCell
+                align="left"
+                style={
+                  s.Status === "PENDING"
+                    ? { color: "blue" }
+                    : s.Status === "APPROVED"
+                    ? { color: "green" }
+                    : { color: "red" }
+                }
+              >
+                {s.Status}
+              </StyledTableCell>
               <StyledTableCell align="left">
                 <Button
                   variant="contained"

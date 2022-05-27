37fac805f9c69a4b531c60851dece57c09a81e99
amir0123 <amirkarmacharya4@gmail.com>
2022-05-27 16:13:50 +0545

    changes

--- mis/src/index.js
+++ mis/src/index.js
@@ -17,7 +17,6 @@ import { USER_SESSION } from "./constants";
 //   window.location.href = "http://103.90.86.151:100/";
 // }
 
-
 sessionStorage.setItem(
   "blueberrytoken",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNDQ5MTY3NC01M2MwLTQzNmMtYjhlNS05YzMyOThjZTc0NGQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTA1MzMiLCJJRFVzZXIiOiIxMDUzMyIsIklEUm9sZSI6IjIiLCJNYXJrQXNBZG1pbiI6IlRydWUiLCJJREhSQnJhbmNoIjoiMSIsIklERGVwYXJ0bWVudCI6IjEiLCJjb21wYW55IjoiMiIsIklzVGVtcG9yYXJ5U2Vzc2lvbkVuYWJsZWQiOiJGYWxzZSIsIklzTmV3bHlBZGVkIjoiRmFsc2UiLCJJc0RlcGFydG1lbnRIZWFkIjoiRmFsc2UiLCJSZW1lbWJlck1lIjoiRmFsc2UiLCJGdWxsTmFtZSI6IkNyZWF0aW9uICBBY2FkZW15IiwicGlkUmVmRm9yRWRpdCI6IlJvb3RVc2VyIiwiZXhwIjoxNjU0NTA3Njk4LCJpc3MiOiJodHRwOi8vbXlzaXRlLmNvbSIsImF1ZCI6Imh0dHA6Ly9teXNpdGUuY29tIn0.eWQt0waIGNcIX-pdRfv_EAwIQ8kQR5ZYqX50yZp7wUc"
@@ -25,7 +24,7 @@ sessionStorage.setItem(
 sessionStorage.setItem(
   "blueberryrefreshtoken",
   "LrZatIyyC+TH1HyjQ+Yj9XYhSJLYxVKUDq8f5NoI5Rk="
-
+);
 
 ReactDOM.render(
   <Provider store={store}>
